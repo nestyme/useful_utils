4bb63151cb41ed371c46fd54f89d9710e941eadc
Nadezhda Zueva <zueva.nn@phystech.edu>
2019-02-14 00:16:20 +0300

    Update ogg_to_wav.py

--- ogg_to_wav.py
+++ ogg_to_wav.py
@@ -1,7 +1,15 @@
 import librosa
 
+#TUA: ffmpeg package must be installed!
 def ogg_to_wav(ogg_file):
     tmp_audio, sr = librosa.load(ogg_file)
     wavfile = ogg_file.split('.')[0] + '.wav'
     librosa.output.write_wav(wavfile, tmp_audio, sr)
     return wavfile
+
+# parallel usage example
+
+from multiprocessing import Pool
+
+with Pool() as p:
+    print(p.map(ogg_to_wav,tqdm_notebook([file for file in train['wav_filename']])))
