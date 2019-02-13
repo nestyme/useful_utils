a933c4251a9cae07003b1fdc95977347b88a69c1
Nadezhda Zueva <zueva.nn@phystech.edu>
2019-02-14 00:22:40 +0300

    Update MFCC_feature_extractor.py

--- MFCC_feature_extractor.py
+++ MFCC_feature_extractor.py
@@ -1,8 +1,8 @@
 import numpy as np
 import scipy.io.wavfile as wav
 
-#enable librosa if you want to convert .oggs. Be carefull -- it might take much more time
-#import librosa 
+# enable librosa if you want to convert .oggs. Be carefull -- it might take much more time
+# import librosa 
 
 from python_speech_features import mfcc
 
@@ -11,7 +11,7 @@ def audiofile_to_input_vector(audio_filename, numcep, numcontext):
     # Load audio files
     fs, audio = wav.read(audio_filename)
     
-    #audio, fs = librosa.load(audio_filename)
+    # audio, fs = librosa.load(audio_filename)
     # Get mfcc coefficients
     # print('numcep: {}, numcontext: {}'.format(numcontext, numcep))
     features = mfcc(audio,samplerate=fs, numcep=numcep, nfft=512, winlen=0.032, winstep=0.02, winfunc=np.hamming)
@@ -20,5 +20,5 @@ def audiofile_to_input_vector(audio_filename, numcep, numcontext):
     empty_context = np.zeros((numcontext, numcep), dtype=features.dtype)
     features = np.concatenate((empty_context, features, empty_context))
     
-    #print('processed: {}'.format(audio_filename))
+    # print('processed: {}'.format(audio_filename))
     return features
